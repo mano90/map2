d24fae12bca7473debb7546bd87db6074a5ec466
mano90 <antsomanoirina90@gmail.com>
2024-02-15 10:22:59 +0300

    add service changeIcon, getRawDevice, savedevice

--- src/app/services/requests/apicall.service.ts
+++ src/app/services/requests/apicall.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Device } from 'src/app/classes/Device';
 import { Locate } from 'src/app/classes/Locate';
 import { environment } from 'src/environments/environment';
 
@@ -17,5 +18,40 @@ export class ApicallService {
   getOneById(id: number): Observable<Locate[]> {
     const url = environment.backUrl + '/device/getOneById/' + id;
     return this.http.get<Locate[]>(url);
+  }
+
+  getAllRawDevice(): Observable<Device[]> {
+    const url = environment.backUrl + '/device/getAllRaw';
+    return this.http.get<Device[]>(url);
+  }
+
+  changeIcon(id: number, data: FormData) {
+    const url = environment.backUrl + '/device/changeIcon/' + id;
+    return this.http.post<any[]>(url, data);
+  }
+  saveDevice(device: Device) {
+    const url = environment.backUrl + '/device/save';
+    return this.http.post<any[]>(url, device);
+  }
+
+  getDeviceById(id: number): Observable<Device> {
+    const url = environment.backUrl + '/device/getRawOneById/' + id;
+    return this.http.get<Device>(url);
+  }
+  updateCoordinates(
+    id: number,
+    limiteHG: string,
+    limiteHD: string,
+    limiteBD: string,
+    limiteBG: string
+  ) {
+    const data = {
+      limiteHG,
+      limiteHD,
+      limiteBD,
+      limiteBG,
+    };
+    const url = environment.backUrl + '/device/updateCoordinates/' + id;
+    return this.http.post<any[]>(url, data);
   }
 }
