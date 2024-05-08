3405820718815bcff15031d58b0b3fb9214756b3
mano90 <antsomanoirina90@gmail.com>
2024-05-08 15:42:05 +0300

    add route history

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { GeneralSettingsComponent } from './general-settings/general-settings.component';
 import { LayoutComponent } from './layout/layout.component';
 import { DeviceSettingsComponent } from './device-settings/device-settings.component';
+import { DeviceHistoryComponent } from './device-history/device-history.component';
 
 const routes: Routes = [
   { path: '', component: LayoutComponent },
@@ -11,6 +12,7 @@ const routes: Routes = [
     path: 'parametres',
     component: GeneralSettingsComponent,
   },
+  { path: 'device-history/:id', component: DeviceHistoryComponent },
   { path: 'device', component: DeviceSettingsComponent },
 ];
 
